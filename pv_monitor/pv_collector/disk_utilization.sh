073932bff55b19eda9e486aba7f4c4705cd4dfff
creekliu <creekliu@tencent.com>
2021-01-10 19:46:17 +0800

    refactor the pv_collector

--- pv_monitor/pv_collector/disk_utilization.sh
+++ pv_monitor/pv_collector/disk_utilization.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 target=$1
-result=`df --output=pcent,target | grep $target | awk '{print \$1}'`
-echo $result
+cmd="df --output=pcent,target | grep $target | awk '{print \$1}'"
+eval "$cmd"
