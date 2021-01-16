60b4756d3de9430fd70079a332ef77df4b54ff03
creekliu <creekliu@tencent.com>
2021-01-16 20:00:41 +0800

    update hpa_exporter

--- pv_monitor/hpa_exporter/setup.sh
+++ pv_monitor/hpa_exporter/setup.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
 
 # 删除遗留的可执行文件
-rm -rf pv_exporter
+rm -rf hpa_exporter
 echo "Step1: rm SUCCESS."
 
 # 编译生成可执行文件
-go build -o pv_exporter .
+go build -o hpa_exporter .
 echo "Step2: go build SUCCESS."
 
 # 构建镜像
-docker build -t aliuchangjie/pv_exporter .
+docker build -t aliuchangjie/hpa_exporter .
 echo "Step3: docker build SUCCESS."
 
 # 登录
@@ -17,14 +17,14 @@ docker login
 echo "Step4: docker login SUCCESS."
 
 # 将镜像推送到仓库
-docker push aliuchangjie/pv_exporter
+docker push aliuchangjie/hpa_exporter
 echo "Step5: docker push SUCCESS."
 
 # 删除遗留的yaml部署资源
-kubectl delete -f pv_exporter.yaml
+kubectl delete -f hpa-exporter.yaml
 echo "Step6: kubectl delete SUCCESS."
 
 # 部署yaml文件
-kubectl apply -f pv_exporter.yaml
+kubectl apply -f hpa-exporter.yaml
 echo "Step7: kubectl apply SUCCESS."
 
