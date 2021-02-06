b24564e10f866633609f1bdfdc89625585d1c0a3
MapleLiu <changjieliu@ruc.edu.cn>
2021-02-06 15:50:37 +0800

    fix the FileNotFoundException

--- hdfs/benchmark/TestDFSIO.sh
+++ hdfs/benchmark/TestDFSIO.sh
@@ -15,11 +15,11 @@ curdir=`pwd`
 
 for ((runs=1; runs<=10; runs++))
 do
-  mkdir testdfsio_log_$runs
-  hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -write -nrFiles 1 -size $size"MB" -resFile $curdir/testdfsio_log_$runs/TestDFSIO-write-$nrFiles-1-$size.log
-  for ((i=1; i<$nrFiles; i++))
+  mkdir testdfsio_log_"$runs"
+  hadoop jar "${HADOOP_HOME}"/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -write -nrFiles 1 -size $size"MB" -resFile "$curdir"/testdfsio_log_$runs/TestDFSIO-write-$nrFiles-1-$size.log
+  for ((i=1; i<"$nrFiles"; i++))
   do
-    hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -append -nrFiles 1 -size $size"MB" -resFile $curdir/testdfsio_log_$runs/TestDFSIO-append-$nrFiles-$i-$size.log
+    hadoop jar "${HADOOP_HOME}"/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -append -nrFiles 1 -size $size"MB" -resFile "$curdir"/testdfsio_log_$runs/TestDFSIO-append-$nrFiles-$i-$size.log
   done
   hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -clean
 done
