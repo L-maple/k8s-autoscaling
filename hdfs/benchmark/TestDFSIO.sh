7a28f527302607e1e611645dc7071c2bffb45e0e
MapleLiu <changjieliu@ruc.edu.cn>
2021-02-06 15:11:32 +0800

    fix the write process bash

--- hdfs/benchmark/TestDFSIO.sh
+++ hdfs/benchmark/TestDFSIO.sh
@@ -2,21 +2,24 @@
 
 
 # hadoop benchmark脚本文件（手动）
-#       
-a=$1
-b=$2
-c=$3
+#
+nrFiles=$1
+size=$2
+
 curdir=`pwd`
-# command: ./hdfs-TestDFSIO.sh  <write/read> <nrFiles> <size>
-#       eg: ./hdfs-TestDFSIO.sh write 10 128
+# command: ./hdfs-TestDFSIO.sh  write-append <nrFiles> <size>
 #       size: 单位MB
   
 #----------------------------TestDFSIO----------------------------#
 
 mkdir TestDFSIO_log
 
-#for i in {1..$b}
-for ((i=1; i<=$b; i++))
+for ((runs=1; runs<=10; runs++))
 do
-  hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -$a -nrFiles 1 -size $c"MB" -resFile $curdir/TestDFSIO_log/TestDFSIO-$a-$i-$c.log
+  hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -write -nrFiles 1 -size $size"MB" -resFile $curdir/testdfsio_log_$runs/TestDFSIO-write-$nrFiles-1-$size.log
+  for ((i=1; i<$nrFiles; i++))
+  do
+    hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -append -nrFiles 1 -size $size"MB" -resFile $curdir/testdfsio_log_$runs/TestDFSIO-append-$nrFiles-$i-$size.log
+  done
+  hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -clean
 done
