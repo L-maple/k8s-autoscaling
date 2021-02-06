3739ab91bbc96da6fc8790b3618ce365048afc78
MapleLiu <changjieliu@ruc.edu.cn>
2021-02-06 11:55:55 +0800

    update TestDFSIO.sh

--- hdfs/benchmark/TestDFSIO.sh
+++ hdfs/benchmark/TestDFSIO.sh
@@ -18,5 +18,5 @@ mkdir TestDFSIO_log
 #for i in {1..$b}
 for ((i=1; i<=$b; i++))
 do
-  hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -$a -nrFiles $b -size $c"MB" -resFile $curdir/TestDFSIO_log/TestDFSIO-$a-$b-$c.log
+  hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -$a -nrFiles $b -size $c"MB" -resFile $curdir/TestDFSIO_log/TestDFSIO-$a-$i-$c.log
 done
