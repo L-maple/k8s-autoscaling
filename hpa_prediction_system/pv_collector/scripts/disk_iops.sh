d617d41e9bd59d517bd5229b75da492cd40500a7
MapleLiu <changjieliu@ruc.edu.cn>
2021-02-22 21:50:44 +0800

    fix the iops error

--- hpa_prediction_system/pv_collector/scripts/disk_iops.sh
+++ hpa_prediction_system/pv_collector/scripts/disk_iops.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-cmd="iostat -N | grep $1 | awk '{print \$2}'"
+#cmd="iostat -N | grep $1 | awk '{print \$2}'"
+#eval "$cmd"
+
+device_name="lvdisplay -c | grep $1 |  awk -F ':' '{print 'dm-'\$13}'"
+cmd="iostat | grep $device_name | awk '{print \$2}'"
 eval "$cmd"
