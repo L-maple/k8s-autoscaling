c835b5f5d270cd16e26b77c7891a716536b9c4ba
MapleLiu <changjieliu@ruc.edu.cn>
2021-02-23 09:30:33 +0800

    fix the disk_iops bug

--- hpa_prediction_system/pv_collector/scripts/disk_iops.sh
+++ hpa_prediction_system/pv_collector/scripts/disk_iops.sh
@@ -6,6 +6,6 @@
 # shellcheck disable=SC2140
 device_name_command="lvdisplay -c | grep $1 |  awk -F ':' '{print \$13}'"
 dm_name="dm-"$(eval "$device_name_command")
-echo "$dm_name"
+#echo "$dm_name"
 cmd="iostat | grep '$dm_name' | awk '{print \$2}'"
 eval "$cmd"
--- hpa_prediction_system/pv_collector/scripts/disk_read_kbps.sh
+++ hpa_prediction_system/pv_collector/scripts/disk_read_kbps.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
 
-cmd="iostat -N | grep $1 | awk '{print \$3}'"
+# shellcheck disable=SC2140
+device_name_command="lvdisplay -c | grep $1 |  awk -F ':' '{print \$13}'"
+dm_name="dm-"$(eval "$device_name_command")
+#echo "$dm_name"
+cmd="iostat | grep '$dm_name' | awk '{print \$3}'"
 eval "$cmd"
