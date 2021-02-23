97311288988b38cc83536997aa28e088e648efa0
MapleLiu <changjieliu@ruc.edu.cn>
2021-02-23 09:25:20 +0800

    fix the disk_iops bug

--- hpa_prediction_system/pv_collector/scripts/disk_iops.sh
+++ hpa_prediction_system/pv_collector/scripts/disk_iops.sh
@@ -3,7 +3,8 @@
 #cmd="iostat -N | grep $1 | awk '{print \$2}'"
 #eval "$cmd"
 
-device_name_command="lvdisplay -c | grep $1 |  awk -F ':' '{print 'dm-'\$13}'"
+# shellcheck disable=SC2140
+device_name_command="lvdisplay -c | grep $1 |  awk -F ':' '{print "dm-"\$13}'"
 dm_name=$(eval "$device_name_command")
 echo "dm_name:", "$dm_name"
 cmd="iostat | grep '$dm_name' | awk '{print \$2}'"
