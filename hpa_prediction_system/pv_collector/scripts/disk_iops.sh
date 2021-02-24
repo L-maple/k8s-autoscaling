a67ced2cd14853cd48499e9d3f2c9402fe6ec7c7
MapleLiu <changjieliu@ruc.edu.cn>
2021-02-24 13:38:04 +0800

    update the scripts

--- hpa_prediction_system/pv_collector/scripts/disk_iops.sh
+++ hpa_prediction_system/pv_collector/scripts/disk_iops.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-cmd="iostat -N | grep $1 | awk '{print \$2}'"
-eval "$cmd"
+#cmd="iostat -N | grep $1 | awk '{print \$2}'"
+#eval "$cmd"
 
 # shellcheck disable=SC2140
-#device_name_command="lvdisplay -c | grep $1 |  awk -F ':' '{print \$13}'"
-#dm_name="dm-"$(eval "$device_name_command")
-##echo "$dm_name"
-#cmd="iostat | grep '$dm_name' | awk '{print \$2}'"
-#eval "$cmd"
+device_name_command="lvdisplay -c | grep $1 |  awk -F ':' '{print \$13}'"
+dm_name="dm-"$(eval "$device_name_command")
+#echo "$dm_name"
+cmd="iostat | grep '$dm_name' | awk '{print \$2}'"
+eval "$cmd"
--- hpa_prediction_system/pv_collector/scripts/disk_write_kbps.sh
+++ hpa_prediction_system/pv_collector/scripts/disk_write_kbps.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-cmd="iostat -N | grep $1 | awk '{print \$4}'"
-eval "$cmd"
-
-## shellcheck disable=SC2140
-#device_name_command="lvdisplay -c | grep $1 |  awk -F ':' '{print \$13}'"
-#dm_name="dm-"$(eval "$device_name_command")
-##echo "$dm_name"
-#cmd="iostat | grep '$dm_name' | awk '{print \$4}'"
+#cmd="iostat -N | grep $1 | awk '{print \$4}'"
 #eval "$cmd"
+
+# shellcheck disable=SC2140
+device_name_command="lvdisplay -c | grep $1 |  awk -F ':' '{print \$13}'"
+dm_name="dm-"$(eval "$device_name_command")
+#echo "$dm_name"
+cmd="iostat | grep '$dm_name' | awk '{print \$4}'"
+eval "$cmd"
