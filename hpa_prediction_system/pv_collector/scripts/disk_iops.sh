a050b4bc6db6636e19bbf09d69041942140e0932
MapleLiu <changjieliu@ruc.edu.cn>
2021-02-24 13:52:37 +0800

    update the scripts

--- hpa_prediction_system/pv_collector/scripts/disk_iops.sh
+++ hpa_prediction_system/pv_collector/scripts/disk_iops.sh
@@ -1,11 +1,12 @@
 #! /bin/bash
 
-#cmd="iostat -N | grep $1 | awk '{print \$2}'"
-#eval "$cmd"
-
 # shellcheck disable=SC2140
 device_name_command="lvdisplay -c | grep $1 |  awk -F ':' '{print \$13}'"
 dm_name="dm-"$(eval "$device_name_command")
-#echo "$dm_name"
-cmd="iostat | grep '$dm_name' | awk '{print \$2}'"
-eval "$cmd"
+if [ "$dm_name" == "dm-" ]
+then
+  echo ""
+else
+  cmd="iostat | grep '$dm_name' | awk '{print \$2}'"
+  eval "$cmd"
+fi
--- hpa_prediction_system/pv_collector/scripts/disk_read_kbps.sh
+++ hpa_prediction_system/pv_collector/scripts/disk_read_kbps.sh
@@ -6,6 +6,10 @@
 # shellcheck disable=SC2140
 device_name_command="lvdisplay -c | grep $1 |  awk -F ':' '{print \$13}'"
 dm_name="dm-"$(eval "$device_name_command")
-#echo "$dm_name"
-cmd="iostat | grep '$dm_name' | awk '{print \$3}'"
-eval "$cmd"
+if [ "$dm_name" == "dm-" ]
+then
+  echo ""
+else
+  cmd="iostat | grep '$dm_name' | awk '{print \$3}'"
+  eval "$cmd"
+fi
