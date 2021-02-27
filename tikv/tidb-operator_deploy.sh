1ca7a4ebfc099a84682ec0e6705f37edb787ad27
Maple <changjieliu@ruc.edu.cn>
2021-02-27 07:15:16 -0500

    update tikv

--- tikv/tidb-operator_deploy.sh
+++ tikv/tidb-operator_deploy.sh
@@ -1,20 +1,18 @@
 #! /bin/bash
 
-chart_version="v1.1.11"
+echo "STEP1 create crd"
+kubectl apply -f manifests/crd.yaml
 
-echo "STEP1 create CRD"
-kubectl apply -f https://raw.githubusercontent.com/pingcap/tidb-operator/v1.1.11/manifests/crd.yaml
+echo "STEP2 download the operator"
+mkdir -p ${HOME}/tidb-operator && \
+helm inspect values pingcap/tidb-operator --version=v1.1.11 > ./tidb-operator/values-tidb-operator.yaml
 
-echo "STEP2 download the values-tidb-operator.yaml."
-mkdir -p tidb-operator && \
-helm inspect values pingcap/tidb-operator --version=${chart_version} > tidb-operator/values-tidb-operator.yaml
-
-echo "STEP3 configure the image source"
-sed -i 's?k8s.gcr.io?registry.cn-hangzhou.aliyuncs.com/google_containers?g' tidb-operator/values-tidb-operator.yaml
+echo "STEP3 configure the image"
+sed -i 's?k8s.gcr.io?registry.cn-hangzhou.aliyuncs.com/google_containers?g' ./tidb-operator/values-tidb-operator.yaml
 
 echo "STEP4 deploy the tidb-operator"
 kubectl create namespace tidb-admin
-helm install tidb-operator pingcap/tidb-operator --namespace=tidb-admin --version=${chart_version} -f tidb-operator/values-tidb-operator.yaml 
+helm install tidb-operator pingcap/tidb-operator --namespace=tidb-admin --version=v1.1.11 -f ./tidb-operator/values-tidb-operator.yaml 
 
 sleep 5
 
--- tikv/tidb-operator_remove.sh
+++ tikv/tidb-operator_remove.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
 echo "STEP1 delete crd resources."
-kubectl delete -f https://raw.githubusercontent.com/pingcap/tidb-operator/v1.1.11/manifests/crd.yaml
+kubectl delete -f manifests/crd.yaml
 
 echo "STEP2 delete tidb-admin namespace"
+# helm uninstall tidb-operator 
+
 kubectl delete namespace tidb-admin
