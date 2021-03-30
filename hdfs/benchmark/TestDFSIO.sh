fac0b1d76fb8d5349a89ac32ec332654a55aedbd
MapleLiu <changjieliu@ruc.edu.cn>
2021-03-30 15:52:21 +0800

    update time TestDFSIO.sh

--- hdfs/benchmark/TestDFSIO.sh
+++ hdfs/benchmark/TestDFSIO.sh
@@ -12,11 +12,18 @@ curdir=`pwd`
   
 #----------------------------TestDFSIO----------------------------#
 
+# shellcheck disable=SC2046
+start_time=$(date +%s)
+echo "start time: " "$start_time"
 mkdir testdfsio_log_"$nrFiles"_"$size"
 hadoop jar "${HADOOP_HOME}"/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -write -nrFiles 1 -size $size"MB" -resFile "$curdir"/testdfsio_log_"$nrFiles"_"$size"/TestDFSIO-write-$nrFiles-1-$size.log
 for ((i=1; i<"$nrFiles"; i++))
 do
   hadoop jar "${HADOOP_HOME}"/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -append -nrFiles 1 -size $size"MB" -resFile "$curdir"/testdfsio_log_"$nrFiles"_"$size"/TestDFSIO-append-$nrFiles-$i-$size.log
 done
+# shellcheck disable=SC2046
+end_time=$(date +%s)
+echo "HDFS start time: " "$start_time"
+echo "HDFS end time:   " "$end_time"
 # hadoop jar ${HADOOP_HOME}/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -clean
 
--- hpa_prediction_system/hpa_exporter/timer.go
+++ hpa_prediction_system/hpa_exporter/timer.go
@@ -101,7 +101,7 @@ func (d *DiskUtilizationTimer) IsStress(podCounter int, aboveCeilingNumber int, 
 func (d *DiskUtilizationTimer) Run() {
 	d.avgDiskUtilizationBoundary = 0.6
 	d.availabilityBottomBoundary = 0.6
-	d.availabilityUpperBoundary = 0.85
+	d.availabilityUpperBoundary  = 0.9
 
 	for {
 		// 状态从 Free 到 Stress
