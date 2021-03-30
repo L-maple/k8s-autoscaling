daf5d413f10b3cffc45c3b2f31f279412e42f730
MapleLiu <changjieliu@ruc.edu.cn>
2021-03-30 19:09:51 +0800

    add time function

--- hdfs/benchmark/TestDFSIO.sh
+++ hdfs/benchmark/TestDFSIO.sh
@@ -17,9 +17,12 @@ start_time=$(date +%s)
 echo "start time: " "$start_time"
 mkdir testdfsio_log_"$nrFiles"_"$size"
 hadoop jar "${HADOOP_HOME}"/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -write -nrFiles 1 -size $size"MB" -resFile "$curdir"/testdfsio_log_"$nrFiles"_"$size"/TestDFSIO-write-$nrFiles-1-$size.log
+# shellcheck disable=SC2005
+echo "write: $(date +%s)" >> time.log
 for ((i=1; i<"$nrFiles"; i++))
 do
   hadoop jar "${HADOOP_HOME}"/share/hadoop/mapreduce/hadoop-mapreduce-client-jobclient-2.9.0-tests.jar TestDFSIO -append -nrFiles 1 -size $size"MB" -resFile "$curdir"/testdfsio_log_"$nrFiles"_"$size"/TestDFSIO-append-$nrFiles-$i-$size.log
+  echo "append $i: $(date +%s)" >> time.log
 done
 # shellcheck disable=SC2046
 end_time=$(date +%s)
